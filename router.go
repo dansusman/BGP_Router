25b810a05107ae42889e8ffa9ee4c0af5bf308ea
ja0911 <ja0911@gmail.com>
2021-10-10 13:35:59 -0400

    some progress

--- router.go
+++ router.go
@@ -1,13 +1,38 @@
 package main
 
 import (
-	"flag"
+	"bufio"
+	"bytes"
+	"encoding/json"
 	"fmt"
+	"io"
 	"net"
 	"os"
 	"strconv"
 	"strings"
 )
+
+// Reading the given connection until a newline character
+func readAll(conn net.Conn) (message []byte, err error) {
+	// creating a buffer store the accumulator
+	var buf bytes.Buffer
+	// NewReader creates reader of default size
+	scanner := bufio.NewReader(conn)
+
+	// looping through the response to ensure the entire message is read
+	for {
+		tmp, e := scanner.ReadString('\n')
+		if e != nil {
+			if e != io.EOF {
+				return "", e
+			}
+			break
+		}
+		// adding the bytes read in this iteration to the buffer
+		print(tmp)
+	}
+	return buf.Bytes(), nil
+}
 
 const DEBUG bool = false
 
@@ -39,14 +64,50 @@ const PEER string = "peer"
 const PROV string = "prov"
 
 type Router struct {
-	routes    []string
-	updates   []string
+	// List of IP Addresses mapped to port number (index)
+	routes []string
+	// Copy of each update announcement in JSON
+	updates []map[string]string
+	// IP Address to type of relationship (Customer, peer, or provider)
 	relations map[string]string
-	sockets   map[string]net.Conn
+	// IP Address mapped to its open connection
+	sockets map[string]net.Conn
+}
+
+type Network struct {
+	network string
+	netmask string
+	netType string
+	AS      string
+}
+
+type Msg struct {
+	network    string
+	netmask    string
+	localpref  int
+	ASPath     []int
+	origin     string
+	selfOrigin bool
+}
+
+type Message struct {
+	mType string
+	src   string
+	dest  string
+	msg   Msg
+}
+
+type Packet struct {
+	asn      int
+	networks []Network
+	messages []Message
 }
 
 func main() {
-	asn, networks := parseInput()
+	asn, networks := parseCmdArgs()
+	println(asn)
+	router := makeRouter(networks)
+	srcif := ""
 	for {
 		//     socks = select.select(self.sockets.values(), [], [], 0.1)[0]
 		//     for conn in socks:
@@ -65,12 +126,32 @@ func main() {
 		//         else:
 		//             return
 		// return
-
+		socks := router.sockets
+		for ip, conn := range socks {
+			message, err := readAll(conn)
+			checkError(err)
+			println(message)
+			if message != nil {
+				if socks[ip] == conn {
+					srcif = ip
+				}
+				var packet Packet
+				err := json.Unmarshal(message, &packet)
+				checkError(err)
+				println(packet)
+				if !handle_packet(srcif, packet) {
+					send_error(conn, packet.messages)
+				} else {
+					return
+				}
+			}
+		}
+		return
 	}
 
 }
 
-func parseInput() (int, []string) {
+func parseCmdArgs() (int, []string) {
 	if len(os.Args) < 2 {
 		panic("Please specify an ASN for your router!")
 	}
@@ -125,16 +206,16 @@ func checkError(err error) {
 
 // Lookup all valid routes for an address
 func lookup_routes(daddr string) []string {
-
+	return nil
 }
 
 // Select the route with the shortest AS Path
 func get_shortest_as_path(routes []string) string {
-
+	return ""
 }
 
 func get_highest_preference(routes []string) string {
-
+	return ""
 }
 
 // Select self originating routes
@@ -174,7 +255,7 @@ func get_route(srcif string, daddr string) {
 
 }
 
-func forward(srcif string, packet string) {
+func forward(srcif string, packet Packet) {
 
 }
 
@@ -182,22 +263,23 @@ func coalesce() {
 
 }
 
-func update(srcif string, packet string) {
+func update(srcif string, packet Packet) {
 
 }
 
-func revoke(packet string) {
+func revoke(packet Packet) {
 
 }
 
-func dump(packet string) {
+func dump(packet Packet) {
 
 }
 
-func handle_packet(srcif string, packet string) {
+func handle_packet(srcif string, packet Packet) bool {
+	return true
 
 }
 
-func send_error(conn net.Conn, msg string) {
+func send_error(conn net.Conn, msg []Message) {
 
 }
